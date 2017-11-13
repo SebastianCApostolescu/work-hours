147863a57c79b361a91758884af1e405b32cade8
SebastianCApostolescu <sebastianc.apostolescu@gmail.com>
2017-11-13 13:34:42 +0100

    Styiling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './styles/App.css';
+import React, { Component } from 'react'
+import './styles/App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Check your working hours!</h1>
+				</header>
+				<div className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</div>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
